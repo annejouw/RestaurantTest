7921cd5eda57486104698a0fa0c84179e2ab0388
basRet <s.j.h.reterink@students.uu.nl>
2022-03-30 20:39:31 +0200

    -Added some placeholder code for the actual functioning -Added some more testing junk code to try to load a fruit image

--- public/javascripts/newMenu.js
+++ public/javascripts/newMenu.js
@@ -11,4 +11,24 @@ let menuContent = document.getElementById("menu-content")
 let fruitPic = document.createElement("img");
 fruitPic.setAttribute("src", '/images/fruits.jpg');
 
-menuContent.appendChild(fruitPic);
+//sends request for new menu items json
+menuItemshttpRequest = new XMLHttpRequest();
+menuItemshttpRequest.onreadystatechange = function (){
+    if (menuItemshttpRequest.readyState === 4 && menuItemshttpRequest.status === 200){
+        var response = JSON.parse(menuItemshttpRequest.responseText)
+    }
+}
+menuItemshttpRequest.open('POST', 'Router url', true)
+menuItemshttpRequest.send("new page nigiri")
+
+//test
+imagehttpRequest = new XMLHttpRequest();
+imagehttpRequest.onreadystatechange = function (){
+    if (menuItemshttpRequest.readyState === 4 && menuItemshttpRequest.status === 200){
+        //image received
+        menuContent.appendChild(fruitPic);
+    }
+}
+
+imagehttpRequest.open('GET', "/images/fruits.jpg", true)
+imagehttpRequest.send();
