3a94069823e8abbd6cd12431394ecc354bd8b395
basRet <s.j.h.reterink@students.uu.nl>
2022-04-05 21:42:16 +0200

    -Gepoogd om database tables te creeeren

--- app.js
+++ app.js
@@ -40,32 +40,35 @@ function closeDatabase() {
         if (err) {
           console.error(err.message);
         }
-        console.log('Close the database connection.');
+        else
+        console.log('Closed the database connection.');
     });
 }
 
-openDatabase();
 //creates Tables for database
-db.serialize(function() {
     if (!databaseExists) {
-        db.run("CREATE TABLE users (userID INTEGER PRIMARY KEY, firstName TEXT NOT NULL, lastName TEXT NOT NULL, email TEXT NOT NULL UNIQUE, phone TEXT NOT NULL, streetAddress TEXT NOT NULL, zipCode TEXT NOT NULL, city TEXT NOT NULL, password TEXT NOT NULL)");
-        createDefaultUsers();
-        openDatabase(); //since insertDefaultUsers closes the database at the end
-        db.run("CREATE TABLE maki (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
-        db.run("CREATE TABLE nigiri (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
-        db.run("CREATE TABLE sashimi (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
-        db.run("CREATE TABLE dessert (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
-        db.run("CREATE TABLE drink (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
-        //session info table, relates session ID's with user ID's when logging in, marks user as anonymous by default
-        //db.run("CREATE TABLE sessionInfo (sessionId INT PRIMARY KEY NOT NULL, userId INTEGER, userType TEXT DEFAULT 'anonymous', date DATE DEFAULT GETDATE() )");
-        //table used when logging orders, uses sessionId as the user type (and ID if logged in) will be defined in the sessionInfo table
-        //db.run("CREATE TABLE orders (orderId INTEGER PRIMARY KEY, sessionId INTEGER NOT NULL, foodItem TEXT NOT NULL, itemCount INTEGER NOT NULL)");
-        //last table which relates orders to users and logs the date
-        //db.run("CREATE TABLE orderHistory (userId INTEGER NOT NULL, orderId INTEGER NOT NULL UNIQUE, date DATE DEFAULT GETDATE(), PRIMARY KEY(userId, date) )");
-        //db.run("INSERT INTO users (firstName, lastName, email, phone, password) VALUES ('Annemijn', 'van Koten', 'annemijnvankoten@gmail.com', '0639224616', 'test')")
-        closeDatabase();
-    }
-});
+        openDatabase();
+        db.serialize(() => {
+            db.run("CREATE TABLE users (userID INTEGER PRIMARY KEY, firstName TEXT NOT NULL, lastName TEXT NOT NULL, email TEXT NOT NULL UNIQUE, phone TEXT NOT NULL, streetAddress TEXT NOT NULL, zipCode TEXT NOT NULL, city TEXT NOT NULL, password TEXT NOT NULL)");
+            createDefaultUsers();
+            openDatabase(); //since insertDefaultUsers closes the database at the end
+            console.log("1");
+            db.run("CREATE TABLE maki (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
+            console.log("2");
+            db.run("CREATE TABLE nigiri (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
+            db.run("CREATE TABLE sashimi (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
+            db.run("CREATE TABLE dessert (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
+            db.run("CREATE TABLE drink (dishID INTEGER PRIMARY KEY, dishName TEXT NOT NULL, price TEXT NOT NULL, imageURL TEXT NOT NULL, ingredients TEXT NOT NULL, vegetarian TEXT NOT NULL)");
+            //session info table, relates session ID's with user ID's when logging in, marks user as anonymous by default
+            //db.run("CREATE TABLE sessionInfo (sessionId INT PRIMARY KEY NOT NULL, userId INTEGER, userType TEXT DEFAULT 'anonymous', date DATE DEFAULT GETDATE() )");
+            //table used when logging orders, uses sessionId as the user type (and ID if logged in) will be defined in the sessionInfo table
+            //db.run("CREATE TABLE orders (orderId INTEGER PRIMARY KEY, sessionId INTEGER NOT NULL, foodItem TEXT NOT NULL, itemCount INTEGER NOT NULL)");
+            //last table which relates orders to users and logs the date
+            //db.run("CREATE TABLE orderHistory (userId INTEGER NOT NULL, orderId INTEGER NOT NULL UNIQUE, date DATE DEFAULT GETDATE(), PRIMARY KEY(userId, date) )");
+            //db.run("INSERT INTO users (firstName, lastName, email, phone, password) VALUES ('Annemijn', 'van Koten', 'annemijnvankoten@gmail.com', '0639224616', 'test')")
+            closeDatabase();
+    });
+};
 
 
 //function to add default users
@@ -80,7 +83,7 @@ function insertDefaultUser(firstName, lastName, email, phone, streetAddress, zip
 };
 
 //function to add maki dishes
-function createMakiDishes(dishID, dishName, price, imageURL, ingredients, vegetarian){
+function insertMakiDishes(dishID, dishName, price, imageURL, ingredients, vegetarian){
     let insertQuery = "INSERT INTO maki (dishID, dishName, price, imageURL, ingredients, vegetarian) VALUES (?, ?, ?, ?, ?, ?)";
     db.run(insertQuery, [dishID, dishName, price, imageURL, ingredients, vegetarian], (err) => {
         if (err) {
@@ -91,7 +94,7 @@ function createMakiDishes(dishID, dishName, price, imageURL, ingredients, vegeta
 };
 
 //
-function insertMakiDishes(){
+function createMakiDishes(){
     openDatabase();
     db.serialize(function (){
     })
@@ -100,15 +103,15 @@ function insertMakiDishes(){
 //inserting default users in DB
 function createDefaultUsers(){
     openDatabase();
-    db.serialize(function() {
+    db.serialize(() => {
         insertDefaultUser('Annemijn', 'van Koten', 'annemijnvankoten@gmail.com', '0639224616', 'Heemstedelaan 18', '3523KE', 'Utrecht', 'Annemijn1234');
         insertDefaultUser('Martijn', 'Hannosset', 'martijnhannosset@gmail.com', '0640889850', 'Van der Haveweg 128', '4411RB', 'Rilland', 'Martijn1234');
         insertDefaultUser('Jeff', 'Tatum', 'jefftatum@gmail.com', '0694201337', 'Steenhouwer 8', '9502ET', 'Stadskanaal', 'Jeff1234');
         insertDefaultUser('Bas', 'Ret', 'basret@gmail.com', '0622394616', 'Zuidzijde 69', '2411RS', 'Bodegraven', 'BasRet123');
         insertDefaultUser('Fleur', 'van Koten', 'fleurvankoten@gmail.com', '0639546506', 'Paxlaan 15', '2613GC', 'Delft', 'Fleur1234');
+        closeDatabase();
     });
-    closeDatabase();
-}
+};
 
 /* 
 Middleware
