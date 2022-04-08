3f1545b1b32650f8d41bca046d7c92a40866a943
Annemijn van Koten <annemijnvankoten@gmail.com>
2022-04-08 14:28:17 +0200

    Removed unnecessary elements

--- public/javascripts/newMenu.js
+++ public/javascripts/newMenu.js
@@ -1,40 +1,18 @@
 const menuContent = document.getElementById('menu-content');
 
-
-let dishHTTPRequest = new XMLHttpRequest();
-dishHTTPRequest.onreadystatechange = function (){
-    if (dishHTTPRequest.readyState === 4 && dishHTTPRequest.status === 200){
-        //Dish received
-        console.log(dishHTTPRequest.responseText);
-        // console.log("dish received")
-        // dishJSON = JSON.parse(dishHTTPRequest.responseText)
-        // addDishToPage(dishJSON)
-
-
-    }
-}
-
-//creates listeners that send appropriate XMLHttpRequest for menu links
-let menuLinkArray = document.querySelectorAll(".menu__link")
-menuLinkArray.forEach(menuLink => addMenuLinkListener(menuLink))
+//creates listeners that send appropriate ajax request for menu links
+let menuLinkArray = document.querySelectorAll(".menu__link");
+menuLinkArray.forEach(menuLink => addMenuLinkListener(menuLink));
 
 function addMenuLinkListener(menuLink){
-    menuLink.addEventListener("click", menuLinkEventHandler)
+    menuLink.addEventListener("click", menuLinkEventHandler, false);
 }
-
-/*
--
--Uses the value property of the html element to determine the Category to send to the server with an http server
-*/
 
 function menuLinkEventHandler(evt){
     let menuLinkElement = evt.target; //this is the button html element
     let requestedCategory = menuLinkElement.value; //this is the value string in the button, which should be the same as the table name on server
 
     let HTTPRequestURL = '/dish/' + requestedCategory;
-
-    /*dishHTTPRequest.open('POST', HTTPRequestURL, true);
-    dishHTTPRequest.send();*/
 
     $.ajax({  
         url:HTTPRequestURL,  
